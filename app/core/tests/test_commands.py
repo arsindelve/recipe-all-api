b1df6e2728ff515e2c00a9b958ae01b54584e7c1
Michael Lane <michael.lane@cart.com>
2023-09-13 12:53:21 -0500

    Changes

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -10,7 +10,7 @@ from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
-@patch('django.db.utils.ConnectionHandler.__getitem__')
+@patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
 
@@ -20,7 +20,7 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        self.assertEqual(patched_getitem.call_count, 1)
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_getitem):
@@ -30,4 +30,4 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        self.assertEqual(patched_getitem.call_count, 6)
+        patched_check.assert_called_with(databases=['default'])
