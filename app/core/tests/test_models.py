0b6afde40266a09e39b1608a3052585104dfda7e
Michael Lane <michael.lane@cart.com>
2023-09-16 08:52:27 -0500

    Changes

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,9 +1,11 @@
 """
 Tests for models
 """
-
+from decimal import Decimal
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
 
 
 class ModelTests(TestCase):
@@ -42,3 +44,14 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_superuser('test@example.com', 'test123')
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        user = get_user_model().objects.create_user('test@example.com', 'test123', )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample Recipe',
+            time_minutes=5,
+            price=Decimal('10.00'),
+            description='Sample description'
+        )
+        self.assertEquals(str(recipe), recipe.title)
